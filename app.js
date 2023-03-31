b0945758207acbe33c0f0b6e1d157f59840bb405
kamroberts <77961944+kamroberts@users.noreply.github.com>
2023-03-31 05:04:20 +0000

    changes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function sendMail() {
     var subject = document.getElementById('subject').value;
     var phone = 'Phone: ' + document.getElementById('phone').value;
     var email = 'Email: ' + document.getElementById('email').value;
-    var message = 'Message:' + document.getElementById('message').value;
+    var message = 'Message: ' + document.getElementById('message').value;
 
     window.location = "mailto:ben__scott1@hotmail.ca?subject=" + subject + '&body=' + body + '%0D%0A' + email + '%0D%0A' + phone + '%0D%0A' + '%0D%0A' + message;
 }
